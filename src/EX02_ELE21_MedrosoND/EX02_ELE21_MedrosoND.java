17bfc4e354134ed0d28bf8ae3ecfb9c94901d6e2
Jerrold Berana <Jerrold Berana@DESKTOP-7A9G3F0>
2023-11-10 18:35:13 +0800

    EX02_ELE21_MedrosoND

---
+++ src/EX02_ELE21_MedrosoND/EX02_ELE21_MedrosoND.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex02_ele21_medrosond;
+
+/**
+ *
+ * @author phetamine
+ */
+public class EX02_ELE21_MedrosoND {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
